cc6245d6a6f78b06b4dd58eeb3f166cb128ccf23
karishma gangwani <karishma.gangwani07@gmail.com>
2026-02-11 13:58:44 +0530

    change pharma example image

--- src/pages/CaseStudies.tsx
+++ src/pages/CaseStudies.tsx
@@ -160,7 +160,7 @@ export function CaseStudies() {
         "Informed strategic portfolio decisions for pain & oncology therapeutics",
       ],
       images:
-        "https://images.unsplash.com/photo-1613843439331-2080752b4518?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwcm9kdWN0JTIwZGV2ZWxvcG1lbnQlMjB0ZWNobm9sb2d5fGVufDF8fHx8MTc3MDcxMzYzNXww&ixlib=rb-4.1.0&q=80&w=1080",
+        "/images/cell-culture.png",
     },
   ];
 
